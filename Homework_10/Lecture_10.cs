ac6c62fe62a0ef19939a4cae37f02754f0c3d611
oleksii.hurov <oleksii.hurov@g,ail.com>
2023-05-11 16:16:24 +0200

    changes in Lecture_10

--- Homework_10/Lecture_10.cs
+++ Homework_10/Lecture_10.cs
@@ -4,6 +4,12 @@
     {
         public int Add(int x, int y) { return x + y; }
 
+        public int Sub(int x, int y) { return x - y; }
+
+        public int Mul(int x, int y) { return x * y; }
+
+        public int Div(int x, int y) { return x / y; }
+
         public static void Main() { }
     }
 }
