ecf9200c9ad257acd32663ef31ddc48256c6cf72
oleksii.hurov <oleksii.hurov@g,ail.com>
2023-05-29 20:46:45 +0200

    Homework_13

---
+++ Homework_13/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Homework_13
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
