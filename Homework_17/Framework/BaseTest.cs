6034f1ed1eb024c2f0bb5b95758d9eda9d558bad
oleksii.hurov <oleksii.hurov@g,ail.com>
2023-09-03 18:27:11 +0200

    Homework_17

---
+++ Homework_17/Framework/BaseTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+
+namespace Homework_17.Framework
+{
+	[TestFixture]
+	public class BaseTest
+	{
+		protected IWebDriver drv;
+		protected WebDriverWait wait;
+		protected static string testUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL", EnvironmentVariableTarget.User);
+		protected static string testUser = Environment.GetEnvironmentVariable("ENT_QA_USER", EnvironmentVariableTarget.User);
+		protected static string testPass = Environment.GetEnvironmentVariable("ENT_QA_PASS", EnvironmentVariableTarget.User);
+		protected static string testCompany = Environment.GetEnvironmentVariable("ENT_QA_COMPANY", EnvironmentVariableTarget.User);
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArgument("start-maximized");
+			drv = new ChromeDriver(options);
+			wait = new WebDriverWait(drv, TimeSpan.FromSeconds(5));
+			drv.Navigate().GoToUrl($"{testUrl}/CorpNet/Login.aspx");
+			LoginToCorpNet();
+		}
+
+		protected void LoginToCorpNet()
+		{
+			wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("username")))
+				.SendKeys(testUser);
+			drv.FindElement(By.Id("password"))
+				.SendKeys(testPass);
+			drv.FindElement(By.Name("_companyText"))
+				.SendKeys(testCompany);
+			drv.FindElement(By.ClassName("login-submit-button"))
+				.Click();
+			wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.page-title")));
+		}
+
+		[OneTimeTearDown]
+		public void ShutDown()
+		{
+			drv.Quit();
+		}
+	}
+}
---
+++ Homework_17/Tests/ReportsTest.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.WaitHelpers;
+using Homework_17.Framework;
+
+
+namespace Homework_17.Tests
+{
+	public class ReportsTest : BaseTest
+	{
+		private string _reportName = "";
+
+		private void NavigateToReportsListPageByUrl()
+		{
+			drv.Navigate().GoToUrl($"{testUrl}/corpnet/report/reportlist.aspx");
+			var pageTitle = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("page-title")));
+			Assert.That(pageTitle.Text, Is.EqualTo("REPORTS"));
+		}
+
+		private void ClickOnTheFirstReportInTheList()
+		{
+			var locator = By.CssSelector("table tr:nth-child(1) td.gc-ReportsListGrid-DisplayAs a");
+			var reportNameLocator = wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+			_reportName = reportNameLocator.Text;
+			reportNameLocator.Click();
+
+			var modalDialog = By.CssSelector("div[data-role='generatereportdialog']");
+			wait.Until(ExpectedConditions.ElementIsVisible(modalDialog));
+		}
+
+		private void SelectToggleShowReportHeader()
+		{
+			var locator = By.XPath(
+				"//div[contains(@class,'widget-label')]//label[contains(text(),'Show Report Header')]/.." +
+				"/following-sibling::div//span[contains(@class,'k-switch-container')]"
+			);
+			var toogleLocator = drv.FindElement(locator);
+			toogleLocator.Click();
+		}
+
+		private void ClickOnGenerateReportButton()
+		{
+			var buttonLocator = drv.FindElement(By.ClassName("id-generate-button"));
+			buttonLocator.Click();
+		}
+
+		private void SwitchToReportResultsTab()
+		{
+			var windowHandlesCount = drv.WindowHandles.Count;
+			wait.Until(drv => drv.WindowHandles.Count > windowHandlesCount);
+			drv.SwitchTo().Window(drv.WindowHandles.Last());
+			wait.Until(drv => drv.FindElement(By.CssSelector("[aria-label='Report table']")));
+		}
+
+		private void VerifyReportResultsTitle()
+		{
+			Assert.That(drv.Title, Is.EqualTo(_reportName));
+		}
+
+		private void CloseGeneratedReportTab()
+		{
+			drv.Close();
+		}
+
+		private void CloseGenerateReportDialog()
+		{
+			drv.SwitchTo().Window(drv.WindowHandles.First());
+			var buttonLocator = drv.FindElement(By.ClassName("id-btn-close"));
+			buttonLocator.Click();
+		}
+
+		[Test, Order(1)]
+		public void GenerateReport()
+		{
+			NavigateToReportsListPageByUrl();
+			ClickOnTheFirstReportInTheList();
+			SelectToggleShowReportHeader();
+			ClickOnGenerateReportButton();
+			SwitchToReportResultsTab();
+			VerifyReportResultsTitle();
+			CloseGeneratedReportTab();
+			CloseGenerateReportDialog();
+		}
+	}
+}
