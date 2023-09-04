ba327982f1ff4914685fb23aaa6768a8cd3701a4
oleksii.hurov <oleksii.hurov@g,ail.com>
2023-09-04 12:45:54 +0200

    removed old SANDBOX

--- SANDBOX_Tests/SANDBOX_Tests.cs
+++ SANDBOX_Tests/SANDBOX_Tests.cs
@@ -1,24 +1,67 @@
-using SANDBOX;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Threading;
 
 namespace SANDBOX_Tests
 {
-    public class Tests
-    {
-        CalcCore calc;
+	public class Tests
+	{
+		protected IWebDriver drv;
+		protected WebDriverWait wait;
+		protected static string _BaseUrl = Environment.GetEnvironmentVariable("ENT_QA_BASE_URL", EnvironmentVariableTarget.User);
+		protected static string _User = Environment.GetEnvironmentVariable("ENT_QA_USER", EnvironmentVariableTarget.User);
+		protected static string _Password = Environment.GetEnvironmentVariable("ENT_QA_PASS", EnvironmentVariableTarget.User);
+		protected static string _Company = Environment.GetEnvironmentVariable("ENT_QA_COMPANY", EnvironmentVariableTarget.User);
 
-        [SetUp]
-        public void Setup()
-        {
-            calc = new CalcCore();
-        }
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.AddArgument("start-maximized");
+			drv = new ChromeDriver(options);
+			wait = new WebDriverWait(drv, TimeSpan.FromSeconds(5));
+			drv.Navigate().GoToUrl($"{_BaseUrl}/CorpNet/Login.aspx");
+			LoginToCorpNet();
+		}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-            //Console.WriteLine($"Result: {calc.Add(2, 2)}");
-            Assert.That(calc.Add(2, 2), Is.EqualTo(4));
-        }
-    }
+		protected void LoginToCorpNet()
+		{
+			wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("username")))
+				.SendKeys(_User);
+			drv.FindElement(By.Id("password"))
+				.SendKeys(_Password);
+			drv.FindElement(By.Name("_companyText"))
+				.SendKeys(_Company);
+			drv.FindElement(By.ClassName("login-submit-button"))
+				.Click();
+			wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div.page-title")));
+		}
+
+		[OneTimeTearDown]
+		public void ShutDown()
+		{
+			drv.Quit();
+		}
+
+		private void NavigateToPage()
+		{
+			drv.Navigate().GoToUrl($"{_BaseUrl}/corpnet/common/todolist.aspx");
+		}
+
+		private void OpenDialog()
+		{
+			drv.FindElement(By.ClassName("area-action background-border icon-add")).Click();
+		}
+
+		[Test, Order(1)]
+		public void Test()
+		{
+			NavigateToPage();
+			OpenDialog();
+			Thread.Sleep(3000); // DEBUG //
+		}
+	}
 }
